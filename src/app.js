a0ac533ab48bccb7575eea36609ba9b045b358f5
Lucas Ferreira <lucasfsouza12@gmail.com>
2020-04-11 22:08:58 -0300

    feat: Created get,post,put,delete for repositories

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid,isUuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +10,72 @@ app.use(cors());
 
 const repositories = [];
 
+function validateProjectId(req,res,next){
+  const {id} = req.params;
+
+  if(!isUuid(id)){
+    return res.status(400).json({error: 'Invalid repository ID.'});
+  }
+
+  return next();
+}
+
+app.use('/repositories/:id', validateProjectId);
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {title, url, techs} = request.body;
+
+  const repository = {id: uuid(),title, url, techs, likes: 0};
+
+  repositories.push(repository);
+
+  return response.json(repository)  
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  const {title, url, techs} = request.body;
+
+  const repositorytIndex = repositories.findIndex(rep => rep.id === id);
+
+  if(repositorytIndex < 0){
+    return response.status(400).json({error: 'Repository not found.'});
+  }
+  let repository = repositories[repositorytIndex];
+  
+  repository = {
+    ...repository,
+    title,
+    url,
+    techs,
+  }
+
+  repositories[repositorytIndex] = repository;
+
+  return response.json(repository)
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  
+  const repositorytIndex = repositories.findIndex(rep => rep.id === id);
+
+  if(repositorytIndex < 0){
+    return response.status(400).json({error: 'Repository not found.'});
+  }
+
+  repositories.splice(repositorytIndex,1);
+
+  return response.status(204).send(repositories);
 });
 
 app.post("/repositories/:id/like", (request, response) => {
   // TODO
 });
+
 
 module.exports = app;
